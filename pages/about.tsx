15a55bb9c95597a0cd3b8064c44f682dff6841ea
DerMatte <me@dermatte.de>
2022-02-17 16:14:32 +0100

    remove lorem on about

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,7 +10,6 @@ const AboutPage = () => (
     <div className="flex flex-col justify-center items-center p-16 bg-pattern-white">
       <h1 className="text-2xl md:text-4xl lg:text-6xl py-10">About</h1>
       <h3>All about us</h3>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla natus quae cumque sunt sit, animi perspiciatis alias eius harum! Soluta officiis omnis commodi aut accusamus quaerat itaque natus animi harum.</p>
     </div>
   </Layout>
 )
