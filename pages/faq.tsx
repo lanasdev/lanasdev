d0dc465a4bfd3f114be18f94e55689513c71dff1
DerMatte <me@dermatte.de>
2022-03-16 15:05:55 +0100

    make heading in faq bigger

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 const FaqPage = () => (
     <Layout title="FAQ">
         <main className="flex flex-col justify-center items-center p-16">
-            <h1 className="text-2xl py-10">FAQ</h1>
+            <h1 className="text-2xl md:text-4xl lg:text-6xl py-10">FAQ</h1>
             <p>This is the FAQ page</p>
         </main>
     </Layout>
