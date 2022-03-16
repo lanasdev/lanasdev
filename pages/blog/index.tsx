2a873fe3df74b30369d8ef4ec4bb1dce748f4411
DerMatte <me@dermatte.de>
2022-03-16 17:56:39 +0100

    change heading on sub pages

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -8,7 +8,7 @@ const BlogIndex = () => {
 
         <Layout title="Home">
             <main className="flex flex-col justify-center items-center p-16">
-                <h1 className="text-2xl py-10">Blog</h1>
+                <h1 className="text-2xl md:text-4xl lg:text-6xl py-10">Blog</h1>
                 <p>This is the Blog page</p>
             </main>
         </Layout>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,7 +6,7 @@ import Contactform from "../components/Contactform";
 const ContactPage = () => (
   <Layout title="Contact">
     <main className="flex flex-col justify-center items-center p-16">
-      <h1 className="text-2xl py-10">Contact</h1>
+      <h1 className="text-2xl md:text-4xl lg:text-6xl py-10">Contact</h1>
       <p>Contact me here: </p>
       <a href="https://t.me/lanasdev" className="text-blue-500 hover:text-blue-700 p-8">Telegram</a>
     </main>
