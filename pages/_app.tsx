396b41bc3418e6d9f14a6aeee9ba91b6ac46f240
DerMatte <me@dermatte.de>
2022-10-30 00:15:39 +0200

    add vc analytics again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="bg-white text-midnight selection:bg-amber-500 dark:bg-midnight dark:text-white ">
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
