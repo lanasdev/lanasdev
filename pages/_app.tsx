9d766337f5d4ff7c0c2dc3efa32f84c93bb05def
DerMatte <me@dermatte.de>
2022-10-29 22:57:52 +0200

    rm simple analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,21 +6,10 @@ import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Fragment>
-      <div className="bg-white text-midnight selection:bg-amber-500 dark:bg-midnight dark:text-white ">
-        <Component {...pageProps} />
-        <Analytics />
-      </div>
-      <Script src="https://sa.lanas.dev/latest.js" />
-      <noscript>
-        {/* eslint-disable @next/next/no-img-element */}
-        <img
-          src="https://sa.lanas.dev/noscript.gif"
-          alt=""
-          referrerPolicy="no-referrer-when-downgrade"
-        />
-      </noscript>
-    </Fragment>
+    <div className="bg-white text-midnight selection:bg-amber-500 dark:bg-midnight dark:text-white ">
+      <Component {...pageProps} />
+      <Analytics />
+    </div>
   );
 }
 
