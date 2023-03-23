3e2780388cc1199c9a23f285f62c02901879b965
DerMatte <me@dermatte.de>
2023-03-23 22:17:48 +0100

    fix dumb scroll issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html className="scroll-smooth subpixel-antialiased">
+    <Html className="subpixel-antialiased">
       <Head></Head>
       <Main />
       <NextScript />
