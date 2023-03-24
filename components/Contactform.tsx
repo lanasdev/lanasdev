c9f849f8efb4ec786d43af1d6503c9ecf09d8fae
DerMatte <me@dermatte.de>
2023-03-24 20:29:49 +0100

    fix text color in form

--- components/Contactform.tsx
+++ components/Contactform.tsx
@@ -144,7 +144,7 @@ const Contactform = ({ locale }) => {
                 id="message"
                 minLength={4}
                 rows={4}
-                className="block w-full rounded-md border-0 py-2 px-3.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-amber-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-2 px-3.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-amber-600 dark:text-midnight sm:text-sm sm:leading-6"
                 defaultValue={""}
               />
               <ValidationError
