1bd50d4a4c5c6fba97c019d2e3750cb7ba0d71bc
DerMatte <me@dermatte.de>
2023-06-05 02:25:06 +0200

    add call redirect

--- next.config.js
+++ next.config.js
@@ -4,5 +4,14 @@ const nextConfig = {
   images: {
     domains: ["www.datocms-assets.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/call",
+        destination: "https://cal.com/lanas/hallo",
+        permanent: false,
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
