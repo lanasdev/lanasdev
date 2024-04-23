1897bcbb0cce87a262e17333e04c07d12324d954
Matthias <matthias@lanas.dev>
2024-04-23 12:34:14 +0200

    improved contact

--- components/ContactButton.tsx
+++ components/ContactButton.tsx
@@ -3,18 +3,27 @@
 import { useFormStatus } from "react-dom";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { ArrowRight } from "@phosphor-icons/react";
 
-export default function ContactButton() {
+export default function ContactButton({
+  className,
+}: {
+  className: string | null;
+}) {
   const { pending } = useFormStatus();
   return (
     <button
       type="submit"
-      className={cn(buttonVariants({ variant: "default" }), "group mt-8")}
+      className={cn(
+        buttonVariants({ variant: "default" }),
+        "group mt-8",
+        className,
+      )}
       aria-disabled={pending}
     >
       Jetzt durchstarten{" "}
-      <span className=" ml-2 transition-transform group-hover:translate-x-1">
-        {"->"}
+      <span className="ml-2 transition-transform group-hover:translate-x-1">
+        <ArrowRight className="" />
       </span>
     </button>
   );
