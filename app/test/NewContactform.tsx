66775e704b3c960274b35e762a3baa9ac627a78f
Matthias <matthias@lanas.dev>
2025-09-01 09:14:43 +0200

    up & rm about image

--- app/test/NewContactform.tsx
+++ app/test/NewContactform.tsx
@@ -8,156 +8,158 @@ import * as z from "zod";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { toast } from "@/components/ui/use-toast";
 
 const profileFormSchema = z.object({
-  name: z
-    .string()
-    .min(2, {
-      message: "Your Name must be at least 2 characters.",
-    })
-    .max(30, {
-      message: "Your Name must not be longer than 30 characters.",
-    }),
-  surname: z
-    .string()
-    .min(2, {
-      message: "Your Name must be at least 2 characters.",
-    })
-    .max(30, {
-      message: "Your Name must not be longer than 30 characters.",
-    }),
-  email: z
-    .string({
-      required_error: "Please select an email to display.",
-    })
-    .email(),
-  website: z.string().url({ message: "Please enter a valid URL." }).optional(),
-  message: z.string().max(1000).min(4),
+	name: z
+		.string()
+		.min(2, {
+			message: "Your Name must be at least 2 characters.",
+		})
+		.max(30, {
+			message: "Your Name must not be longer than 30 characters.",
+		}),
+	surname: z
+		.string()
+		.min(2, {
+			message: "Your Name must be at least 2 characters.",
+		})
+		.max(30, {
+			message: "Your Name must not be longer than 30 characters.",
+		}),
+	email: z
+		.string({
+			message: "Please enter an email address.",
+		})
+		.email({
+			message: "Please enter a valid email address.",
+		}),
+	website: z.string().url({ message: "Please enter a valid URL." }).optional(),
+	message: z.string().max(1000).min(4),
 });
 
 type ProfileFormValues = z.infer<typeof profileFormSchema>;
 
 // This can come from your database or API.
 const defaultValues: Partial<ProfileFormValues> = {
-  message: "Dear Matthias, ",
-  // website: "https://lanas.dev",
+	message: "Dear Matthias, ",
+	// website: "https://lanas.dev",
 };
 
 export function NewContactform() {
-  const form = useForm<ProfileFormValues>({
-    resolver: zodResolver(profileFormSchema),
-    defaultValues,
-    mode: "onChange",
-  });
+	const form = useForm<ProfileFormValues>({
+		resolver: zodResolver(profileFormSchema),
+		defaultValues,
+		mode: "onChange",
+	});
 
-  function onSubmit(data: ProfileFormValues) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
-    console.log(data);
-  }
+	function onSubmit(data: ProfileFormValues) {
+		toast({
+			title: "You submitted the following values:",
+			description: (
+				<pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+					<code className="text-white">{JSON.stringify(data, null, 2)}</code>
+				</pre>
+			),
+		});
+		console.log(data);
+	}
 
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <div className="flex flex-col sm:flex-row sm:gap-4 sm:*:flex-1">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>First Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="Matthias" {...field} />
-                </FormControl>
-                <FormDescription>First Name</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="surname"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="Lanas" {...field} />
-                </FormControl>
-                <FormDescription>Last Name</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <Input placeholder="hey@lanas.dev" {...field} />
-              <FormDescription>Your work Email</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="website"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Website</FormLabel>
-              <Input placeholder="https://lanas.dev" {...field} />
-              <FormDescription>
-                Your current Website. Only input if you already have one
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="message"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Message</FormLabel>
-              <FormControl>
-                <Textarea placeholder="" className="resize-y" {...field} />
-              </FormControl>
-              <FormDescription>
-                Tell us a little bit more about the company and what issues you
-                are facing with
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Get a fast Website</Button>
-      </form>
-    </Form>
-  );
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+				<div className="flex flex-col sm:flex-row sm:gap-4 sm:*:flex-1">
+					<FormField
+						control={form.control}
+						name="name"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>First Name</FormLabel>
+								<FormControl>
+									<Input placeholder="Matthias" {...field} />
+								</FormControl>
+								<FormDescription>First Name</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="surname"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Name</FormLabel>
+								<FormControl>
+									<Input placeholder="Lanas" {...field} />
+								</FormControl>
+								<FormDescription>Last Name</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
+				<FormField
+					control={form.control}
+					name="email"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Email</FormLabel>
+							<Input placeholder="hey@lanas.dev" {...field} />
+							<FormDescription>Your work Email</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="website"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Website</FormLabel>
+							<Input placeholder="https://lanas.dev" {...field} />
+							<FormDescription>
+								Your current Website. Only input if you already have one
+							</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="message"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Message</FormLabel>
+							<FormControl>
+								<Textarea placeholder="" className="resize-y" {...field} />
+							</FormControl>
+							<FormDescription>
+								Tell us a little bit more about the company and what issues you
+								are facing with
+							</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type="submit">Get a fast Website</Button>
+			</form>
+		</Form>
+	);
 }
--- components/home/AboutSection.tsx
+++ components/home/AboutSection.tsx
@@ -1,80 +1,79 @@
 import SectionContainer from "@/app/SectionContainer";
 import Link from "next/link";
 import {
-  Image as DatoImage,
-  ResponsiveImageType,
-  StructuredText,
-  renderNodeRule,
+	Image as DatoImage,
+	ResponsiveImageType,
+	StructuredText,
+	renderNodeRule,
 } from "react-datocms";
 
 import { isLink } from "datocms-structured-text-utils";
 
 type AboutSectionProps = {
-  aboutData: {
-    titleAbout: string;
-    textAbout: any;
-    imageAbout: {
-      responsiveImage: ResponsiveImageType;
-    };
-  };
+	aboutData: {
+		titleAbout: string;
+		textAbout: any;
+		imageAbout: {
+			responsiveImage: ResponsiveImageType;
+		};
+	};
 };
 
 const AboutSection = ({
-  aboutData: { titleAbout, textAbout, imageAbout },
+	aboutData: { titleAbout, textAbout, imageAbout },
 }: AboutSectionProps) => {
-  return (
-    <>
-      <SectionContainer className="flex flex-col gap-8 pt-24 md:flex-row">
-        <div className="w-full md:w-2/5 xl:flex-1">
-          <h3 className="pb-8 text-3xl font-semibold">
-            {titleAbout ?? "Über mich"}
-          </h3>
-          <DatoImage
-            data={imageAbout.responsiveImage}
-            className="mb-6 aspect-square rounded-xl sm:max-w-12 md:hidden"
-            objectFit="cover"
-          />
-          <article className="leading-8 text-balance">
-              {/* Render Links as <Link> Tags */}
-              <StructuredText
-                data={textAbout}
-                customNodeRules={[
-                  renderNodeRule(isLink, ({ node, children, key }) => {
-                    return (
-                      <Link
-                        key={key}
-                        href={node.url}
-                        className="text-cyan-700 transition-colors hover:text-accent-foreground hover:underline"
-                      >
-                        {children}
-                      </Link>
-                    );
-                  }),
-                ]}
-              />
-              <br />
-              <Link
-                href="/ueber"
-                className="group block pt-4 text-cyan-700 transition-colors hover:text-accent-foreground hover:underline"
-              >
-                Mehr über mich{" "}
-                <span className="inline-block transition-transform group-hover:translate-x-1">
-                  {"->"}
-                </span>
-              </Link>
-
-          </article>
-        </div>
-        <div className="w-0 md:w-3/5 xl:flex-1">
-          <DatoImage
+	return (
+		<>
+			<SectionContainer className="flex flex-col gap-8 pt-24 md:flex-row">
+				<div className="w-full md:w-2/5 xl:flex-1">
+					<h3 className="pb-8 text-3xl font-semibold">
+						{titleAbout ?? "Über mich"}
+					</h3>
+					<DatoImage
+						data={imageAbout.responsiveImage}
+						className="mb-6 aspect-square rounded-xl sm:max-w-12 md:hidden"
+						objectFit="cover"
+					/>
+					<article className="leading-8 text-balance">
+						{/* Render Links as <Link> Tags */}
+						<StructuredText
+							data={textAbout}
+							customNodeRules={[
+								renderNodeRule(isLink, ({ node, children, key }) => {
+									return (
+										<Link
+											key={key}
+											href={node.url}
+											className="text-cyan-700 transition-colors hover:text-accent-foreground hover:underline"
+										>
+											{children}
+										</Link>
+									);
+								}),
+							]}
+						/>
+						<br />
+						<Link
+							href="/ueber"
+							className="group block pt-4 text-cyan-700 transition-colors hover:text-accent-foreground hover:underline"
+						>
+							Mehr über mich{" "}
+							<span className="inline-block transition-transform group-hover:translate-x-1">
+								{"->"}
+							</span>
+						</Link>
+					</article>
+				</div>
+				<div className="w-0 md:w-3/5 xl:flex-1">
+					{/* <DatoImage
             data={imageAbout.responsiveImage}
             className="hidden rounded-xl md:block object-cover"
             pictureClassName="object-cover "
-          />
-        </div>
-      </SectionContainer>
-    </>
-  );
+          /> */}
+				</div>
+			</SectionContainer>
+		</>
+	);
 };
 
 export default AboutSection;
