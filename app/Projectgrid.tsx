24f8141f40c76385cc57af04fafebcae31509db9
Matthias <matthias@lanas.dev>
2025-10-26 10:52:56 +0100

    Projectgrid is now a grid again and fixed Bloglist group hover

--- app/Projectgrid.tsx
+++ app/Projectgrid.tsx
@@ -31,31 +31,34 @@ export default function Projectgrid({
   const ImgArray = [Image1, Image2, Image3, Image1, Image2, Image3];
 
   return (
-    <SectionContainer className="flex max-w-fit flex-col gap-12 pt-32 md:gap-24">
+    <SectionContainer className=" pt-32 md:gap-24">
       <div id="projekte" />
       <h3 className="hidden">Projekte</h3>
 
-      {allProjects.map((project: ProjectType, index) => (
-         <Link
-          href={`/projekt/${project.slug}`}
-          key={project.position}
-          className="group relative"
-        >
-          <DatoImage
-            data={project?.image?.responsiveImage}
-            priority={index === 0 ? true : false}
-            // layout="fill"
-            objectFit="cover"
-            objectPosition="50% 0%"
-            className="aspect-golden-vertical sm:aspect-3/1 rounded-xl border-2 border-muted-foreground transition-all duration-150 ease-in-out group-hover:scale-103 group-hover:border-foreground group-hover:opacity-80 min-w-full"
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {allProjects.map((project: ProjectType, index) => (
+          <Link
+            href={`/projekt/${project.slug}`}
+            key={project.position}
+            className="group relative"
+          >
+            <DatoImage
+              data={project?.image?.responsiveImage}
+              priority={index === 0 ? true : false}
+              // layout="fill"
+              objectFit="cover"
+              objectPosition="50% 0%"
+              className="aspect-golden-vertical sm:aspect-4/3 rounded-xl border-2 border-muted-foreground transition-all duration-150 ease-in-out group-hover:scale-103 group-hover:border-foreground group-hover:opacity-80 min-w-full"
             // pictureClassName="object-cover object-top"
-          />
-          <p className="line-clamp-2 pt-6 uppercase text-sm sm:text-md">{project.description}</p>
-          <h4 className="text-xl font-semibold group-hover:underline group-hover:decoration-muted-foreground ">
-            {project.title}
-          </h4>
-        </Link>
-      ))}
+            />
+            <p className="line-clamp-2 pt-6 uppercase text-sm sm:text-md">{project.description}</p>
+            <h4 className="text-xl font-semibold group-hover:underline group-hover:decoration-muted-foreground ">
+              {project.title}
+            </h4>
+          </Link>
+        ))}
+      </div>
+
       {/* <pre>{JSON.stringify(allProjects, null, 2)}</pre> */}
     </SectionContainer>
   );
--- components/Bloglist.tsx
+++ components/Bloglist.tsx
@@ -13,23 +13,24 @@ export default function Bloglist({ allPosts }) {
         <h3 className="sr-only">Unser Blog</h3>
       </div>
       <h3 className="pb-8 text-3xl font-semibold">Blog</h3>
-      <div className="flex shrink-0 snap-x space-x-4 overflow-x-auto py-4">
+      <div className="flex shrink-0 snap-x gap-8 overflow-x-auto py-4 px-4 -mx-4">
         {allPosts.map((p: any) => (
           <Link
             href={`/blog/${p.slug}`}
             key={p.slug}
-            className="max-w-xs shrink-0 snap-center"
+            className="w-80 shrink-0 snap-center group relative"
           >
             <DatoImage
               data={p.coverImage.responsiveImage}
-              className="aspect-golden max-h-96 w-full rounded-xl"
+              className="aspect-golden max-h-96 w-full rounded-xl transition-all duration-150 ease-in-out group-hover:scale-103 group-hover:border-foreground group-hover:opacity-80"
               pictureClassName="object-cover"
             />
-            <p className="line-clamp-1 max-w-xs pt-4">{p.excerpt}</p>
-            <h4 className="text-xl font-semibold">{p.title}</h4>
+            <p className="line-clamp-1 pt-4">{p.excerpt}</p>
+            <h4 className="text-xl font-semibold group-hover:underline group-hover:decoration-muted-foreground">{p.title}</h4>
           </Link>
         ))}
       </div>
     </SectionContainer>
   );
 }
+
